4b7a755f73f587050b9141c8dab0fba8e4869b89
adarsh6672 <adarshkeruvatty@gmail.com>
2023-10-02 10:05:06 +0530

    firstcartupdation

--- src/main/java/com/firstcart_ecommerce/firstcart/model/Order.java
+++ src/main/java/com/firstcart_ecommerce/firstcart/model/Order.java
@@ -1,4 +1,15 @@
 package com.firstcart_ecommerce.firstcart.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
 }
--- src/main/java/com/firstcart_ecommerce/firstcart/repository/OrderRepo.java
+++ src/main/java/com/firstcart_ecommerce/firstcart/repository/OrderRepo.java
@@ -3,5 +3,5 @@ package com.firstcart_ecommerce.firstcart.repository;
 import com.firstcart_ecommerce.firstcart.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepo extends JpaRepository<Long , Order> {
+public interface OrderRepo extends JpaRepository<Order , Long> {
 }
