225102f6e4540402b0cf3262625941faa21f6021
adarsh6672 <adarshkeruvatty@gmail.com>
2023-11-15 17:03:39 +0530

    finalupdation

--- src/main/java/com/firstcart_ecommerce/firstcart/controller/UserController.java
+++ src/main/java/com/firstcart_ecommerce/firstcart/controller/UserController.java
@@ -414,13 +414,13 @@ public class UserController {
     }
 
 
-    @GetMapping("/wallet")
-    public String openWallet(Model model,Principal principal){
+    @GetMapping("/open/wallet")
+    public String openWallet1(Model model,Principal principal){
         User user=userRepo.findByEmail(principal.getName());
         Wallet wallet=walletService.getOrCreateUserWallet(user);
         model.addAttribute("walletBalance",wallet.getAmount());
         model.addAttribute("pageTitle", "Wallet | User");
-        return "user/ordersuccess";
+        return "user/wallet";
     }
 
     @PostMapping("/placeorder")
